e2e4df0ddbf85251f3f7a575dcb6a2fd1a73ff0b
Aleksey Klokov <lex-oz@yandex.ru>
2020-11-03 20:46:08 +0300

    calculate mile

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int mile = 1;
+        int cost = 3000;
+        int each = 20;
+        int sum = cost * mile / each;
+        System.out.println(sum);
+    }
+}
